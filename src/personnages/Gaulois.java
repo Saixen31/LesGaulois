65c360c42b96df03cd212eb980723289721170fa
Raphael Leguevaques <lgr5307a@ensfsi.onmicrosoft.com>
2026-03-19 11:28:04 +0100

    fin tp1

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,40 +1,40 @@
 package personnages;
-
 public class Gaulois {
 	private String nom;
 	private int force;
-
+	private int effetPotion = 1;
+	
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
 	}
-
 	public String getNom() {
 		return nom;
 	}
-
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "\"" + texte + "\"");
 	}
-
 	private String prendreParole() {
-		return "Le gaulois " + nom + " : ";
+		return "Le Gaulois " + nom + " : ";
 	}
-	
+	@Override
+	public String toString() {
+		return nom;
+	}
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de "
+				+ romain.getNom());
+		romain.recevoirCoup(force * effetPotion / 3);
+		effetPotion--;
+		if(effetPotion < 1) {
+			effetPotion = 1;
+		}
+	}
+	public void boirePotion(int forcePotion) {
+		this.effetPotion = forcePotion;
+	}
 	public static void main(String[] args) {
 		Gaulois asterix = new Gaulois("Astérix", 8);
 		System.out.println(asterix.getNom());
 	}
-
-	
-	public String toString() {
-		return "Gaulois [nom=" + nom + ", force=" + force + "]";
-	}
-	
-	public void frapper(Romain romain) {
-		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
-		romain.recevoirCoup(force / 3);
-	}
-	
-	
 }
---
+++ src/village_gaulois/Village.java
@@ -0,0 +1,20 @@
+package village_gaulois;
+
+public class Village {
+	private String nom;
+	private int nbVillageois = 0;
+	
+	
+	public Village(String nom, int nbVillageois) {
+		this.nom = nom;
+		this.nbVillageois = nbVillageois;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	public String getChef() {
+		return
+	}
+	
+}
