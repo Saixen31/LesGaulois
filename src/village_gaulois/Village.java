8610745a84a9a65361f75e6dcd2cfb0c57b214e6
Raphael Leguevaques <lgr5307a@ensfsi.onmicrosoft.com>
2026-03-19 12:00:28 +0100

    1.c tp2

--- src/village_gaulois/Village.java
+++ src/village_gaulois/Village.java
@@ -1,20 +1,37 @@
 package village_gaulois;
+import personnages.Gaulois;
 
 public class Village {
 	private String nom;
 	private int nbVillageois = 0;
+	private Gaulois chef;
+	private Gaulois[] villageois;
 	
 	
 	public Village(String nom, int nbVillageois) {
 		this.nom = nom;
 		this.nbVillageois = nbVillageois;
+		this.villageois = new Gaulois[nbVillageois];
 	}
 	
 	public String getNom() {
 		return nom;
 	}
-	public String getChef() {
-		return
+	
+	public Gaulois getChef() {
+		return chef;
 	}
 	
+	public void ajouterVillageois(Gaulois gaulois) {
+		 villageois[nbVillageois] = gaulois;
+		 nbVillageois++;
+	}
+	public Gaulois trouverVillageois(int numVillageois) {
+		int indice = numVillageois -1;
+		if(indice >= 0 && indice < numVillageois) {
+			return villageois[indice];
+		}
+		System.out.println("Il ny'a a pas autant d'habitants !");
+		return null;
+	}
 }
